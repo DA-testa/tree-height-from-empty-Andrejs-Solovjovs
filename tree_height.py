2baca9b751e58eabd1ec8d020d327a3812afb494
Andrejs 221RDB442 <125615728+Andrejs-Solovjovs@users.noreply.github.com>
2023-03-06 01:33:38 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,12 +2,11 @@
 # 221RDB4442 Andrejs Solovjovs
 
 import sys
-import threading
-import numpy as np
 
 def compute_height(n, parents):
-    heights = np.zeros(int(n))
+    heights = [0] * int(n)
     max_height = 0
+
     for i in range(int(n)):
         if heights[i] > 0:
             continue
@@ -23,47 +22,29 @@ def compute_height(n, parents):
         heights[i] = height
         if height > max_height:
             max_height = height
+
     return max_height
 
-def input_from_keyboard():
-    n = input().strip()
-    if n:
-        parents = input().strip().split(" ")
-        if parents:
-            return n, parents
-    return None, None
-
-def input_from_file(file_dir):
+def read_input():
     try:
-        with open(f"./test/{file_dir}") as f:
-            contents = f.readlines()
+        with open(sys.argv[1]) as f:
+            n = f.readline().strip()
+            parents = f.readline().strip().split()
     except:
         print("ERROR")
         return None, None
 
-    n = contents[0].strip()
-    if n:
-        parents = contents[1].strip().split(" ")
-        if parents:
-            f.close()
-            return n, parents
-    return None, None
+    if not n or not parents:
+        print("ERROR")
+        return None, None
+
+    return n, parents
 
 def main():
-    input_method = input().strip()
-    if input_method == "F":
-        file_dir = input().strip()
-        if str(file_dir[-1]) != "a":
-            n, parents = input_from_file(file_dir)
-            if n and parents:
-                height = compute_height(n, parents)
-                print(int(height))
-    elif input_method == "I":
-        n, parents = input_from_keyboard()
-        if n and parents:
-            height = compute_height(n, parents)
-            print(int(height))
+    n, parents = read_input()
+    if n and parents:
+        height = compute_height(n, parents)
+        print(int(height))
 
-sys.setrecursionlimit(10 ** 7) 
-threading.stack_size(2 ** 27) 
-threading.Thread(target=main).start()
+if __name__ == "__main__":
+    main()
